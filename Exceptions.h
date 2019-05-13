01ea51cff77890e25924f4f1f8f2b5ab71664c76
Sharaa <norouzi.sharare@gmail.com>
2019-05-13 16:07:43 +0430

    correct exception semicolon

--- Exceptions.h
+++ Exceptions.h
@@ -4,16 +4,16 @@ class NotFound :public std::exception
 {
 public:
     virtual const char* what() const throw();
-}
+};
 
 class BadRequest :public std::exception
 {
 public:
     virtual const char* what() const throw();
-}
+};
 
 class PermissionDenied :public std::exception
 {
 public:
     virtual const char* what() const throw();
-}
+};
