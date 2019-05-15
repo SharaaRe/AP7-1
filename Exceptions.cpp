d68f9c0b3d5f7a566877ed58d99e2014f976b3b7
Sharaa <norouzi.sharare@gmail.com>
2019-05-15 12:03:05 +0430

    add details to exception

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -1,16 +1,39 @@
+#include <string>
+
 #include "Exceptions.h"
+
+using namespace std;
 
 const char* BadRequest::what() const throw()
 {
-    return "Bad Request";
+    string response = "Bad Request";
+    response += "  details:  " + details;
+    return response.c_str();
+
 }
+
+BadRequest::BadRequest(std::string _detail)
+    :exception(), details(_detail)
+{}
 
 const char* NotFound::what() const throw()
 {
-    return "Not Found";
+    string response = "Not Found";
+    response += "  details:  " + details;
+    return response.c_str();
 }
+
+NotFound::NotFound(std::string _detail)
+    :exception(), details(_detail)
+{}
 
 const char* PermissionDenied::what() const throw()
 {
-    return "Permission Denied";
+    string response = "Permission Denied";
+    response += "  details:  " + details;
+    return response.c_str();
 }
+
+PermissionDenied::PermissionDenied(std::string _detail)
+    :exception(), details(_detail)
+{}
--- Exceptions.h
+++ Exceptions.h
@@ -1,19 +1,30 @@
 #include <exception>
 
+#include <string>
+
 class NotFound :public std::exception
 {
 public:
+    NotFound(std::string detail);
     virtual const char* what() const throw();
+private:
+    std::string details;
 };
 
 class BadRequest :public std::exception
 {
 public:
+    BadRequest(std::string detail);
     virtual const char* what() const throw();
+private:
+    std::string details;    
 };
 
 class PermissionDenied :public std::exception
 {
 public:
+    PermissionDenied(std::string detail);
     virtual const char* what() const throw();
+private:
+    std::string details;    
 };
