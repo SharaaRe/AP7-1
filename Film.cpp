13c26f18ab158b4df29040eec2fcc8c8342bfc9c
Sharaa <norouzi.sharare@gmail.com>
2019-05-15 12:28:48 +0430

    add film class

---
+++ Film.cpp
@@ -0,0 +1,101 @@
+#include "Film.h"
+
+#include "Exceptions.h"
+
+using namespace std;
+
+int Film::last_id_created = 0;
+
+Film::Film(string _name, int _year, int _length, int _price, string _summery, string _director)
+    : name(name), year(_year), length(_length), summery(_summery), director(_director)
+{
+    last_id_created++;
+    id = last_id_created;
+    last_comment_id = 0;
+    rate = 0;
+}
+
+void Film::add_comment(Comment* new_comment)
+{
+    last_comment_id++;
+    comments[id] = new_comment;
+}
+
+
+void Film::reply_comment(int comment_id, string reply_content)
+{
+    if(comments.find(comment_id) == comments.end())
+        throw new NotFound("comment id not found");
+    else    ;
+        //comment should have reply here, complete it later
+}
+
+void Film::delete_comment(int comment_id)
+{
+    if(comments.find(comment_id) == comments.end())
+        throw new NotFound("comment id not found");
+    else
+        comments.erase(comment_id); 
+}
+
+
+void Film::set_name(string _name)
+{
+    name = _name;
+}
+
+string Film::get_name()
+{
+    return name;
+}
+
+void Film::set_year(int _year)
+{
+    year = _year;
+}
+
+int Film::get_year()
+{
+    return year;
+}
+
+void Film::set_year(int _year)
+{
+    year = _year;
+}
+
+int Film::get_year()
+{
+    return year;
+}
+
+void Film::set_summery(string _summery)
+{
+    summery = _summery;
+}
+
+string Film::get_summemry()
+{
+    return summery;
+}
+
+void Film::set_director(string _director)
+{
+    director = _director;
+}
+
+string Film::get_director()
+{
+    return director;
+}
+
+int Film::get_id()
+{
+    return id;
+}
+
+
+
+
+
+
---
+++ Film.h
@@ -0,0 +1,51 @@
+#ifndef FILMS_H
+#define FILMS_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#define NOT_CHANGED "not changed"
+#define VALUE_NOT_CHANGED  -1
+
+class Comment;
+
+class Film
+{
+public:
+    Film(std::string name, int year, int length, int price, std::string summery, std::string director);
+    void add_comment(Comment* new_comment);
+    void reply_comment(int comment_id, std::string reply_content);
+    void delete_comment(int comment_id);
+
+    void set_name(std::string name);
+    std::string get_name();
+    void set_year(int year);
+    int get_year();
+    void set_length(int length);
+    int get_length();
+    void set_summery(std::string summery);
+    std::string get_summemry();
+    void set_director(std::string director);
+    std::string get_director();
+
+    int get_id();
+
+private:
+    static int last_id_created;
+    int id;
+    std::string name;
+    int year;
+    int length; //minutes
+    int price;
+    std::string summery;
+    std::string director;
+
+    std::map <int, Comment*> comments;
+    int last_comment_id;
+    
+
+    float rate;
+};
+
+#endif
