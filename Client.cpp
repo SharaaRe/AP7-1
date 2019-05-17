eae7f6e7011b226107b3a71b8b49ba3a30a42924
Sharaa <norouzi.sharare@gmail.com>
2019-05-17 13:31:12 +0430

    add static id to client

--- Client.cpp
+++ Client.cpp
@@ -1,9 +1,15 @@
 #include "Client.h"
 
+#include "Exceptions.h"
 
-Client::Client(std::string _email, std::string _username, std::string _password, int _age, int _id)
-    :email(_email), username(_username), password(_password), age(_age), id(_id)
-{}
+int Client::last_id_created = 0;
+
+Client::Client(std::string _email, std::string _username, std::string _password, int _age)
+    :email(_email), username(_username), password(_password), age(_age)
+{
+    last_id_created++;
+    id = last_id_created;
+}
 
 
 void Client::follow(int following_id)
@@ -11,6 +17,10 @@ void Client::follow(int following_id)
     followings.push_back(following_id);
 }
 
+void Client::increase_credit(int amount)
+{
+    credit += amount;
+}
 
 void Client::purchase_film(int film_id, int price)
 {
@@ -19,4 +29,26 @@ void Client::purchase_film(int film_id, int price)
     credit -= price;
     purchased.push_back(film_id);
 }
+
+
+void Client::add_film(Film* new_film)
+{
+    throw new PermissionDenied("non publisher client cant add a film");
+}
+
+// void reply_comment(int film_id, int comment_id)
+// {
+//     throw new PermissionDenied("non publisher client cant reply to comments");
+// }
+
+bool film_is_published_by_user(int film_id)
+{
+    throw new PermissionDenied("non publisher clients have not published any films");
+}
+
+std::vector <std::string> get_followers()
+{
+    throw new PermissionDenied("non publisher clients dont have followers");
+}
+
 
--- Client.h
+++ Client.h
@@ -8,16 +8,24 @@
 class Film;
 class Publisher;
 class Notification;
+class Comment;
 
 
 class Client 
 {
 public:
-    Client(std::string email, std::string username, std::string password, int age, int id);
+    Client(std::string email, std::string username, std::string password, int age);
     void follow(int following_id);
     void increase_credit(int amount);
     void purchase_film(int film_id, int price);
+    // void add_comment(Comment* new_comment);
+    virtual void add_film(Film*);
+    virtual void delete_film(int film_id);
+    virtual bool film_is_published_by_user(int film_id);
+    virtual std::vector <std::string> get_followers();
+    virtual void reply_comment(int film_id, int comment_id);
 protected:
+    static int last_id_created;
     int id;
     std::string email;
     std::string username;
