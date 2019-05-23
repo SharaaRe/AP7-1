a7ed0030bbe5b73e8ac8d2aab68acb1029c9b44b
Sharaa <norouzi.sharare@gmail.com>
2019-05-23 12:28:41 +0430

    add replies controller

---
+++ RepliesController.cpp
@@ -0,0 +1,32 @@
+#include "RepliesController.h"
+
+#include "Utils.h"
+#include "FilmService.h"
+#include "Exceptions.h"
+
+
+Response RepliesController::post(Request* request)
+{
+    current_request = request;   
+    check_post_params();
+    FilmService film_service;
+    film_service.reply(film_id, comment_id, content);
+
+    return Response(SUCCESSFUL, OK);
+}
+
+void RepliesController::check_post_params()
+{
+    try
+    {
+        film_id = Utils::string_integer_value(current_request->get_request_param(FILM_ID));
+        comment_id = Utils::string_integer_value(current_request->get_request_param(COMMENT_ID));
+        content = current_request->get_request_param(CONTENT)
+    }
+    catch(NotFound& er)
+    {
+        throw BadRequest("a required params do not exist");
+    }
+    
+
+}
---
+++ RepliesController.h
@@ -0,0 +1,19 @@
+#ifndef REPLIES_CONTROLLER_H
+#define REPLIES_CONTROLLER_H
+
+#include <string>
+
+#include "Controller.h"
+
+class RepliesController:public Controller
+{
+public: 
+    virtual Response post(Request* request);
+private:
+    int film_id;
+    int comment_id;
+    std::string content;
+    void check_post_params();
+};
+
+#endif
